e9b2fb032e8afae50f29a31c1cd7618ba50615c7
Vanconts <rostyslavbur@gmail.com>
2020-08-20 09:42:38 +0200

    fixed errors

--- shared_pointer.hpp
+++ shared_pointer.hpp
@@ -1,5 +1,40 @@
+#include <atomic>
+
 template <typename T>
 class s_ptr{
+private:
+    struct te{ //type erasure idiom
+        std::atomic<unsigned> counter;//for atomic thread-safe operations
+
+        te():counter(1){};
+
+        virtual void Delete() = 0;
+
+        virtual ~te(){};
+    };
+
+    template<typename Orig_Type,typename Deleter_Type>
+    struct deleter : te{ //struct for deleter function handling
+        Orig_Type * orig_pointer;
+        Deleter_Type deleter;
+
+        deleter(Orig_Type * _p,Deleter_Type _d):
+        orig_pointer(_p),
+        deleter(_d){}
+
+        void Delete() const override{
+            deleter(orig_pointer);
+        }
+    };
+
+    template<typename T1>
+    void default_deleter(const T1 * p){ //default delete function
+        delete p;
+    }
+    
+
+    T * p;
+    te * count_del;
 public:
     s_ptr():
     p(nullptr),
@@ -10,38 +45,11 @@ public:
     p(_p),
     count_del(new deleter<T,void(&)()>(p,default_deleter))
     {}
+
+    template<typename Deleter_Type>
+    s_ptr(T * _p, Deleter_Type Deleter):
+    p(_p),
+    count_del(new deleter<T,Deleter_Type>(p,Deleter))
+    {}
     
-
-private:
-    struct te{ //type erasure idiom
-        unsigned counter;
-        te():counter(1){};
-
-        virtual void Delete() = 0;
-
-        virtual ~te(){};
-    };
-
-    template<typename Orig_Type,typename Deleter_Type>
-    struct deleter : te{ //struct for deleter function handling
-        OrigType * orig_pointer;
-        Deleter_type deleter;
-
-        te(OrigType * _p,Deleter_Type _d):
-        orig_pointer(_p),
-        deleter(_d){}
-
-        void Delete() const override{
-            deleter(orig_pointer);
-        }
-    };
-
-    template<typename T>
-    void default_deleter(const T * p){ //default delete function
-        delete p;
-    }
-    
-
-    T * p;
-    te * count_del;
 };
