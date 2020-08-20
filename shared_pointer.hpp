0b20ee9489741898fc4a1f71836973e2c148b574
Vanconts <rostyslavbur@gmail.com>
2020-08-20 09:28:12 +0200

    Added reference counter and deleter function handlers

--- shared_pointer.hpp
+++ shared_pointer.hpp
@@ -1,7 +1,47 @@
 template <typename T>
 class s_ptr{
 public:
+    s_ptr():
+    p(nullptr),
+    count_del()
+    {}
+
+    s_ptr(T * _p):
+    p(_p),
+    count_del(new deleter<T,void(&)()>(p,default_deleter))
+    {}
+    
 
 private:
+    struct te{ //type erasure idiom
+        unsigned counter;
+        te():counter(1){};
+
+        virtual void Delete() = 0;
+
+        virtual ~te(){};
+    };
+
+    template<typename Orig_Type,typename Deleter_Type>
+    struct deleter : te{ //struct for deleter function handling
+        OrigType * orig_pointer;
+        Deleter_type deleter;
+
+        te(OrigType * _p,Deleter_Type _d):
+        orig_pointer(_p),
+        deleter(_d){}
+
+        void Delete() const override{
+            deleter(orig_pointer);
+        }
+    };
+
+    template<typename T>
+    void default_deleter(const T * p){ //default delete function
+        delete p;
+    }
     
+
+    T * p;
+    te * count_del;
 };
